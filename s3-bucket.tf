9aa0b3aff761e8fbee73490d1210a72c6945db3e
Parwinder Kaur <kparwinder756@gmail.com>
2022-06-21 08:15:39 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-  bucket_prefix = parwinderkaur
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/parwinderkaur-training/s3-bucket/aws"
+  version = "2.8.0"
+  bucket_prefix = "parwinderkaur"
 }
